4c4c34846245cdc94d48aacc819ef61b893612c7
artifyfun <artifyfun@gmail.com>
2024-03-28 18:55:52 +0800

    fix: dsl generator

--- antd-tools/dsl/components.js
+++ antd-tools/dsl/components.js
@@ -3,30 +3,35 @@ const fs = require('fs');
 
 const ignoreFiles = [];
 
-// get components all zh-CN md files
-const mdFiles = glob.sync('components/**/index.zh-CN.md*', {
-  ignore: ignoreFiles,
-});
+function getComponents(type = 'zh-CN') {
+  // get components all zh-CN md files
+  const mdFiles = glob.sync(`components/**/index.${type}.md*`, {
+    ignore: ignoreFiles,
+  });
 
-const components = mdFiles.map(file => {
-  const content = fs.readFileSync(file, 'utf8');
-  const headerContent = content.split('---')[1];
-  const headerArr = headerContent
-    .split('\n')
-    .map(item => item.trim())
-    .filter(Boolean);
-  const header = Object.fromEntries(
-    headerArr.map(item => {
-      const [key, value] = item.split(':');
-      return [key.trim(), value?.trim()];
-    }),
-  );
-  const descriptionContent = content.split('---')[2].split('#')[0].trim();
+  const components = mdFiles.map(file => {
+    const content = fs.readFileSync(file, 'utf8');
+    const headerContent = content.split('---')[1];
+    const headerArr = headerContent
+      .split('\n')
+      .map(item => item.trim())
+      .filter(Boolean);
+    const header = Object.fromEntries(
+      headerArr.map(item => {
+        const key = item.split(':')[0];
+        const value = item.split(`${key}:`)[1];
+        return [key.trim(), value?.trim()];
+      }),
+    );
+    const descriptionContent = content.split('---')[2].split('#')[0].trim();
 
-  return {
-    ...header,
-    description: descriptionContent || header.subtitle,
-  };
-});
+    return {
+      ...header,
+      description: descriptionContent || header.subtitle,
+    };
+  });
 
-module.exports = components;
+  return components;
+}
+
+module.exports = getComponents;
--- antd-tools/dsl/tiny-engine-materials.js
+++ antd-tools/dsl/tiny-engine-materials.js
@@ -3,119 +3,219 @@ const pkg = require('../../package.json');
 const { parseAndWrite } = require('../generator-types/lib/index.js');
 const rootPath = path.resolve(__dirname, '../../');
 
+function toKebabCase(camel) {
+  return camel.replace(/((?<=[a-z\d])[A-Z]|(?<=[A-Z\d])[A-Z](?=[a-z]))/g, '-$1').toLowerCase();
+}
+
 const { outputFileSync, readFileSync } = require('fs-extra');
 
 const tagPrefix = '';
 
-const tagDemo = {
-  name: 'collapse',
-  slots: [
-    {
-      name: 'expandIcon',
-      description: '自定义切换图标',
-    },
-  ],
-  events: [],
-  attributes: [
-    {
-      name: 'accordion',
-      default: '`false`',
-      description: '手风琴模式',
-      value: {
-        type: 'boolean',
-        kind: 'expression',
-      },
-    },
-    {
-      name: 'activeKey(v-model)',
-      default: '默认无，accordion 模式下默认第一个元素',
-      description: '当前激活 tab 面板的 key',
-      value: {
-        type: 'string[] | string <br> number[] | number',
-        kind: 'expression',
-      },
-    },
-    {
-      name: 'bordered',
-      default: '`true`',
-      description: '带边框风格的折叠面板',
-      value: {
-        type: 'boolean',
-        kind: 'expression',
-      },
-    },
-    {
-      name: 'collapsible',
-      default: '-',
-      description: '所有子面板是否可折叠或指定可折叠触发区域',
-      value: {
-        type: '`header` | `icon` | `disabled`',
-        kind: 'expression',
-      },
-    },
-    {
-      name: 'destroyInactivePanel',
-      default: '`false`',
-      description: '销毁折叠隐藏的面板',
-      value: {
-        type: 'boolean',
-        kind: 'expression',
-      },
-    },
-    {
-      name: 'expandIcon',
-      default: '',
-      description: '自定义切换图标',
-      value: {
-        type: 'Function(props):VNode | slot="expandIcon" slot-scope="props"|#expandIcon="props"',
-        kind: 'expression',
-      },
-    },
-    {
-      name: 'expandIconPosition',
-      default: '-',
-      description: '设置图标位置',
-      value: {
-        type: '`start` | `end`',
-        kind: 'expression',
-      },
-    },
-    {
-      name: 'ghost',
-      default: 'false',
-      description: '使折叠面板透明且无边框',
-      value: {
-        type: 'boolean',
-        kind: 'expression',
-      },
-    },
-  ],
-};
+function parseAndWriteByType(type = 'zh-CN') {
+  const reg = new RegExp(`${type}.md`);
+  return parseAndWrite({
+    version: pkg.version,
+    name: 'ant-design-vue',
+    path: path.resolve(rootPath, './components'),
+    typingsPath: path.resolve(rootPath, './typings/global.d.ts'),
+    // default match lang
+    test: reg,
+    // test: /zh-CN\.md/,
+    outputDir: path.resolve(rootPath, `./dsl/metadata/${type}`),
+    tagPrefix,
+  });
+}
 
-parseAndWrite({
-  version: pkg.version,
-  name: 'ant-design-vue',
-  path: path.resolve(rootPath, './components'),
-  typingsPath: path.resolve(rootPath, './typings/global.d.ts'),
-  // default match lang
-  // test: /en-US\.md/,
-  test: /zh-CN\.md/,
-  outputDir: path.resolve(rootPath, './dsl/metadata/'),
-  tagPrefix,
-})
-  .then(result => {
-    // eslint-disable-next-line no-console
-    console.log(`generator types success: ${result} tags generated`);
+function getPropertiesContent(attributes) {
+  return attributes
+    .map(attr => {
+      const allowedTypes = ['string', 'number', 'boolean', 'object', 'function'];
+      const formatVariable = word => {
+        let res = word;
+        ["'", '"', '`'].forEach(item => (res = res.replaceAll(item, '')));
+        return res.trim();
+      };
+      const isValidVariable = word =>
+        /^([^\x00-\xff]|[a-zA-Z_$])([^\x00-\xff]|[a-zA-Z0-9_$])*$/.test(formatVariable(word));
 
-    // const webTypes = require(path.resolve(rootPath, './dsl/metadata/web-types.json'));
+      let type = attr.value.type.includes('|')
+        ? attr.value.type
+            .split('|')
+            .map(item => item.trim())[0]
+            .toLowerCase()
+        : attr.value.type.toLowerCase();
+      if (["'", '"', '`'].some(item => type.includes(item)) && isValidVariable(type)) {
+        type = 'string';
+      }
+      if (['CSSProperties', 'array', 'string[]', 'number[]'].includes(type)) {
+        type = 'object';
+      }
+      if (type.toLowerCase().includes('function')) {
+        type = 'function';
+      }
 
-    const components = require('./components.js');
+      if (!allowedTypes.some(item => type.includes(item))) {
+        return null;
+      }
 
-    Object.keys(components).map(name => {
-      const component = components[name];
-      const tag = webTypes.contributions.html.tags.find(tag => tag.name === name);
+      const defaultValue = (() => {
+        if (['无', '-'].includes(attr.default)) {
+          return undefined;
+        }
+        if (['boolean'].includes(type)) {
+          return typeof attr.default === 'string'
+            ? attr.default.includes('true')
+              ? true
+              : attr.default.includes('false')
+              ? false
+              : undefined
+            : undefined;
+        }
+        if (['number'].includes(type)) {
+          return typeof attr.default === 'string' ? Number(attr.default) : attr.default;
+        }
+        if (['object'].includes(type)) {
+          try {
+            return JSON.parse(attr.default);
+          } catch (error) {
+            return undefined;
+          }
+        }
+        return attr.default;
+      })();
+
+      const widget = (() => {
+        if (type === 'boolean') {
+          return {
+            component: 'MetaSwitch',
+            props: {},
+          };
+        }
+        if (type === 'string') {
+          if (
+            attr.value.type.includes('|') &&
+            attr.value.type.split('|').every(item => isValidVariable(item))
+          ) {
+            return {
+              component: 'MetaSelect',
+              props: {
+                options: attr.value.type
+                  .split('|')
+                  .map(item => formatVariable(item))
+                  .map(item => ({
+                    label: item,
+                    value: item,
+                  })),
+              },
+            };
+          }
+          return {
+            component: 'MetaInput',
+            props: {},
+          };
+        }
+        if (type === 'number') {
+          return {
+            component: 'MetaNumber',
+            props: {},
+          };
+        }
+        if (type === 'object') {
+          return {
+            component: 'MetaCodeEditor',
+            props: {
+              language: 'json',
+            },
+          };
+        }
+        if (type === 'function') {
+          return {
+            component: 'MetaCodeEditor',
+            props: {},
+          };
+        }
+      })();
+      return {
+        property: attr.name.replace('(v-model)', '').trim(),
+        label: {
+          text: {
+            zh_CN: attr.name,
+          },
+        },
+        description: {
+          zh_CN: attr.description,
+        },
+        required: false,
+        readOnly: false,
+        disabled: false,
+        cols: 12,
+        labelPosition: 'top',
+        type: type,
+        defaultValue: defaultValue,
+        widget: widget,
+        device: [],
+      };
+    })
+    .filter(Boolean);
+}
+
+function getEvents(events) {
+  return Object.fromEntries(
+    events.map(event => {
+      return [
+        `on${event.name.replace(/(^[a-z])/, char => char.toUpperCase())}`,
+        {
+          label: {
+            zh_CN: event.name,
+          },
+          description: {
+            zh_CN: event.description,
+          },
+          type: 'event',
+          functionInfo: {
+            params: [],
+            returns: {},
+          },
+          defaultValue: '',
+        },
+      ];
+    }),
+  );
+}
+
+function getSlots(slots) {
+  return Object.fromEntries(
+    slots.map(slot => {
+      return [
+        slot.name,
+        {
+          label: {
+            zh_CN: slot.name,
+          },
+          description: {
+            zh_CN: slot.description,
+          },
+        },
+      ];
+    }),
+  );
+}
+
+async function generateMaterials(type = 'zh-CN') {
+  // 先取英文文档才能获取events，slots 等信息
+  await parseAndWriteByType('en-US');
+  const webTypes = require(path.resolve(rootPath, `./dsl/metadata/en-US/web-types.json`));
+
+  const getComponents = require('./components.js');
+  const components = getComponents('en-US');
+
+  const materials = components
+    .map(component => {
+      const tag = webTypes.contributions.html.tags.find(
+        tag => tag.name === toKebabCase(component.title),
+      );
       if (!tag) {
-        console.error(`tag ${name} not found`);
+        console.error(`component ${component.title} not found`);
         return;
       }
       return {
@@ -125,10 +225,10 @@ parseAndWrite({
           zh_CN: component.subtitle,
         },
         component: `A${component.title}`,
-        icon: component.icon || name,
+        icon: component.icon || toKebabCase(component.title),
         description: component.description,
         doc_url: '',
-        screenshot: components.coverDark,
+        screenshot: component.coverDark,
         tags: '',
         keywords: '',
         dev_mode: 'proCode',
@@ -180,152 +280,144 @@ parseAndWrite({
               label: {
                 zh_CN: '基础属性',
               },
-              content: [
-                ...tag.attributes
-                  .map(attr => {
-                    const allowedTypes = ['string', 'number', 'boolean', 'object'];
-                    let type = attr.value.type.includes('|')
-                      ? attr.value.type.split('|').map(item => item.trim())[0]
-                      : attr.value.type;
-                    if (type.includes('`')) {
-                      type = 'string';
-                    }
-                    if (['CSSProperties', 'array'].includes(type)) {
-                      type = 'object';
-                    }
-                    if (!allowedTypes.some(item => type.includes(item))) {
-                      return null;
-                    }
-                    const defaultValue = (() => {
-                      if (['无', '-'].includes(attr.default)) {
-                        return undefined;
-                      }
-                      if (['boolean'].includes(type)) {
-                        return typeof attr.default === 'string'
-                          ? attr.default.includes('true')
-                            ? true
-                            : attr.default.includes('false')
-                            ? false
-                            : undefined
-                          : undefined;
-                      }
-                      if (['number'].includes(type)) {
-                        return typeof attr.default === 'string'
-                          ? Number(attr.default)
-                          : attr.default;
-                      }
-                      if (['object'].includes(type)) {
-                        return typeof attr.default === 'string'
-                          ? JSON.parse(attr.default)
-                          : undefined;
-                      }
-                      return attr.default;
-                    })();
-                    const widget = (() => {
-                      if (type === 'boolean') {
-                        return {
-                          component: 'MetaSwitch',
-                          props: {},
-                        };
-                      }
-                      if (type === 'string') {
-                        return {
-                          component: 'MetaInput',
-                          props: {},
-                        };
-                      }
-                      if (type === 'number') {
-                        return {
-                          component: 'MetaNumber',
-                          props: {},
-                        };
-                      }
-                      if (type === 'object') {
-                        return {
-                          component: 'MetaCodeEditor',
-                          props: {
-                            language: 'json',
-                          },
-                        };
-                      }
-                    })();
-                    return {
-                      property: attr.name,
-                      label: {
-                        text: {
-                          zh_CN: attr.name,
-                        },
-                      },
-                      description: {
-                        zh_CN: attr.description,
-                      },
-                      required: false,
-                      readOnly: false,
-                      disabled: false,
-                      cols: 12,
-                      labelPosition: 'top',
-                      type: type,
-                      defaultValue: defaultValue,
-                      widget: widget,
-                      device: [],
-                    };
-                  })
-                  .filter(Boolean),
-              ],
+              content: getPropertiesContent(tag.attributes),
               description: {
                 zh_CN: '',
               },
             },
           ],
-          events: {},
-          slots: {
-            default: {
-              label: {
-                zh_CN: 'default',
-              },
-              description: {
-                zh_CN: '自定义默认内容',
-              },
-            },
-            loading: {
-              label: {
-                zh_CN: 'loading',
-              },
-              description: {
-                zh_CN: '自定义加载中组件',
-              },
-            },
-          },
+          events: getEvents(tag.events),
+          slots: getSlots(tag.slots),
         },
-        snippets: [
-          {
-            name: {
-              zh_CN: '按钮',
-            },
-            icon: 'button',
-            screenshot: '',
-            snippetName: 'ElButton',
-            schema: {
-              children: [
-                {
-                  componentName: 'Text',
-                  props: {
-                    text: '按钮文本',
-                  },
-                },
-              ],
-            },
-          },
-        ],
+        snippets: [{}],
       };
-    });
+    })
+    .filter(Boolean);
 
-    // outputFileSync(
-    //   path.resolve(rootPath, `./dsl/components/tiny-engine/${component.name}.json`),
-    //   result
-    // );
-  })
-  .catch(error => {
-    console.error('generator types error', error);
-    return Promise.reject(error);
-  });
+  if (type === 'zh-CN') {
+    await parseAndWriteByType('zh-CN');
+    const cnWebTypes = require(path.resolve(rootPath, './dsl/metadata/zh-CN/web-types.json'));
+
+    const getComponents = require('./components.js');
+    const cnComponents = getComponents('zh-CN');
+
+    const cnMaterials = cnComponents
+      .map(component => {
+        const tag = cnWebTypes.contributions.html.tags.find(
+          tag => tag.name === toKebabCase(component.title),
+        );
+        if (!tag) {
+          console.error(`component ${component.title} not found`);
+          return;
+        }
+        const material = materials.find(item => item.component === `A${component.title}`);
+        if (!material) {
+          return;
+        }
+        return {
+          ...material,
+          name: {
+            zh_CN: component.subtitle,
+          },
+          description: component.description,
+          category: component.type,
+          schema: {
+            properties: [
+              {
+                name: '0',
+                label: {
+                  zh_CN: '基础属性',
+                },
+                content: material.schema.properties[0].content.map(item => {
+                  let cnContent = getPropertiesContent(tag.attributes).find(
+                    attr => attr.property === item.property,
+                  );
+                  if (!cnContent) {
+                    return item;
+                  }
+                  return {
+                    ...item,
+                    label: cnContent.label,
+                    description: cnContent.description,
+                  };
+                }),
+                description: {
+                  zh_CN: '',
+                },
+              },
+            ],
+            events: Object.fromEntries(
+              Object.keys(material.schema.events).map(eventName => {
+                const event = material.schema.events[eventName];
+                const name = eventName
+                  .replace('on', '')
+                  .replace(/(^[A-Z])/, char => char.toLowerCase());
+                let cnEvent = getEvents(tag.events)[name];
+                if (!cnEvent) {
+                  cnEvent = getPropertiesContent(tag.attributes).find(
+                    attr => attr.property === name,
+                  );
+                  if (cnEvent) {
+                    cnEvent = {
+                      label: cnEvent.label.text,
+                      description: cnEvent.description,
+                    };
+                  }
+                }
+                if (!cnEvent) {
+                  return [eventName, event];
+                }
+                return [
+                  eventName,
+                  {
+                    ...event,
+                    label: cnEvent.label,
+                    description: cnEvent.description,
+                  },
+                ];
+              }),
+            ),
+            slots: Object.fromEntries(
+              Object.keys(material.schema.slots).map(slotName => {
+                const slot = material.schema.slots[slotName];
+                let cnSlot = getSlots(tag.slots)[slotName];
+                if (!cnSlot) {
+                  cnSlot = getPropertiesContent(tag.attributes).find(
+                    attr => attr.property === slotName,
+                  );
+                  if (cnSlot) {
+                    cnSlot = {
+                      label: cnSlot.label.text,
+                      description: cnSlot.description,
+                    };
+                  }
+                }
+                if (!cnSlot) {
+                  return [slotName, slot];
+                }
+                return [
+                  slotName,
+                  {
+                    ...slot,
+                    label: cnSlot.label,
+                    description: cnSlot.description,
+                  },
+                ];
+              }),
+            ),
+          },
+          snippets: [{}],
+        };
+      })
+      .filter(Boolean);
+
+    cnMaterials.forEach(material => {
+      outputFileSync(
+        path.resolve(rootPath, `./dsl/materials/${material.component}.json`),
+        JSON.stringify(material, null, 2),
+      );
+    });
+  }
+}
+
+generateMaterials('zh-CN');
